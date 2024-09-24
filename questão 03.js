13e8e7e67302abaabc6f9fffaefee35def8bcc63
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2024-09-24 20:37:55 -0300

    att

---
+++ questão 03.js
@@ -0,0 +1,5 @@
+/*Contar Vogais em uma String
+Desenvolva uma função contarVogais(str) que conte e retorne o número de
+vogais (a, e, i, o, u) em uma string fornecida.
+Exemplo: contarVogais('javascript') deve retornar 3 */
+
